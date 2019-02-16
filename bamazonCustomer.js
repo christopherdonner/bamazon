981e3fb3d42381941476b88bc25adb070804febb
christopherdonner <christopher.donner@gmail.com>
2019-02-15 21:37:16 -0500

    more stuff working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,16 +9,28 @@ var connection = mysql.createConnection({
     database: "bamazon"
 })
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected");
 })
 
-connection.query("select item_id, product_name, product_department, price from products order by item_id")
-
-inquirer.prompt([
-    {
-        message: "Please enter the ID of the item you would like to purchase",
-        name: "ID"
+connection.query("select item_id, product_name, department_name, price from products order by item_id", function (err, results) {
+    if (err) { throw err };
+    // console.log(results)
+    for(i=0;i<results.length;i++){
+        console.log(`${results[i].item_id}\n${results[i].product_name}\n${results[i].price}]`)
     }
-])
+
+    inquirer.prompt([
+        {
+            message: "Please enter the ID of the product you would like to purchase:",
+            name: "ID",
+            type: "input",
+            }
+    ]).then(function (res) {
+        connection.query(`select * from product where item_id=${res}`, function (err, results) {
+            if (err) { throw err; }
+            console.log(results)
+        })
+    })
+})
