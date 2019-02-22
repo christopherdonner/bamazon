17912371fb63e6bce5697d457c27fe2a8ee1881c
christopherdonner <43712817+christopherdonner@users.noreply.github.com>
2019-02-21 22:32:45 -0500

    Update seed.sql

--- seed.sql
+++ seed.sql
@@ -14,3 +14,11 @@ values ("birkenstocks", "footwear", 75.00, 4);
 
 insert into products (product_name, department_name, price, quantity)
 values ("binoculars", "optical enhancements", 120.97, 6);
+
+insert into products (product_name, department_name, price, quantity)
+values ("batteries", "portable energy supplies", 18.47, 24);
+
+insert into products (product_name, department_name, price, quantity)
+values ("bananas", "fruit", 0.59, 144);
+
+
